8c9287feb4293bee6f317119eaed01fd6266d072
ZhouQianang <49708236+ZhouQianang@users.noreply.github.com>
2020-07-25 14:18:54 +0800

    Add files via upload

---
+++ stereo_depth/matlab/depth2points.m
@@ -0,0 +1,33 @@
+
+
+intrinsic=[451.607740200530,0.,343.998551683970;
+       0.,457.029505887742,235.146157222543;
+       0.,0.,1. ];
+
+fdx=intrinsic(1,1);
+fdy=intrinsic(2,2);
+u0=intrinsic(1,3);
+v0=intrinsic(2,3);
+
+[h,w] = size(pic2);
+u=repmat(1:w,[h,1]);
+v=repmat(1:h,[w,1])';
+
+f1=imread('./best/leftr/DJI_STE_left_3.jpg');
+
+fg=uint8(zeros(size(f1,1),size(f1,2),3));
+fg(:,:,1)=f1;
+fg(:,:,2)=f1;
+fg(:,:,3)=f1;
+pic2(pic2<0.5)=0.0;
+pic2(pic2>10.0) = 0.0;
+
+Z=pic2;
+X=(Z(:).*(u(:)-u0))/fdx;
+Y=(Z(:).*(v(:)-v0))/fdy;
+
+ptCloud=pointCloud([X(:),Y(:),Z(:)],'Color',reshape(fg,[],3));
+
+ pcshow(ptCloud,'MarkerSize',125);
+
+
---
+++ stereo_depth/matlab/xml2mat.m
@@ -0,0 +1,21 @@
+%function [ pic2 ] = xml2mat( xml_name )
+%clear;
+%读取opencv中xml文件，表示的是矩阵。
+ xmlDoc = xmlread('./best/depth/DJI_STE_depth_3.xml');
+ %得到矩阵的行数
+ row = xmlDoc.getElementsByTagName('rows').item(0).getFirstChild.getData;
+ %得到矩阵的列数
+ col = xmlDoc.getElementsByTagName('cols').item(0).getFirstChild.getData;
+ row = str2num(row);%读入是string类型，转为数字；
+ col = str2num(col);%opencv_
+ %此时读入的是一串字符
+ histstring =char(xmlDoc.getElementsByTagName('data').item(0).getFirstChild.getData);
+
+x1 =strtrim(histstring);%去除首位空格，一般在首位有空格
+x2 = strsplit(x1);%按照空格切分字符
+x3 = str2double(x2);%转为double型
+
+pic1 = reshape(x3,col,row);%转为（col,row）尺寸的mat
+pic2 = pic1';%求转置，这是因为xml文件中的数据是一列一列写入的
+%imshow(pic2,[]);
+%end
